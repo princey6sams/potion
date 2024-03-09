2a1ffe6448761d6f63708c1ea41f8be84769f3dd
Princey6sams <princeasingha@gmail.com>
2024-03-08 23:21:05 -0330

    Added hover-enabled collapse button to sidebar.

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -19,6 +19,12 @@ const Navigation = () => {
       <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]">
         <div
           role="button"
+          className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:hg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition"
+        >
+          <ChevronsLeft className="h-6 w-6" />
+        </div>
+        <div
+          role="button"
           className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition cursor-pointer"
         >
           <ChevronsLeft className="h-6 w-6" />
