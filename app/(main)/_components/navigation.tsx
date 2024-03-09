149f1cd560fc1d92c9bd119235cf912851c0ca23
Princey6sams <princeasingha@gmail.com>
2024-03-08 23:31:49 -0330

    Added dynamic class to sidebar to ensure that it does not show up in mobile view

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -3,6 +3,7 @@ import { ChevronsLeft } from "lucide-react";
 import { usePathname } from "next/navigation";
 import React, { ElementRef, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import { cn } from "../../../lib/utils";
 
 const Navigation = () => {
   const pathname = usePathname();
@@ -16,7 +17,14 @@ const Navigation = () => {
 
   return (
     <>
-      <aside className="group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]">
+      <aside
+        ref={sidebarRef}
+        className={cn(
+          "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "w-0"
+        )}
+      >
         <div
           role="button"
           className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:hg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition"
