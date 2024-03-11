75512c1bfbef7680e8efc62b300e3272bfac5bc6
Princey6sams <princeasingha@gmail.com>
2024-03-10 23:13:19 -0230

    Added avatar from shadcn and adjusted z-index inside dropwdown menu of shadcn ui to match the sidebar.

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { cn } from "../../../lib/utils";
+import { UserItem } from "./user-item";
 
 const Navigation = () => {
   const pathname = usePathname();
@@ -113,7 +114,7 @@ const Navigation = () => {
           <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>Action Items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
