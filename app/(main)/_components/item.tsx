2840915701ad339672f86b6eab18f3de3ae8d53c
Princey6sams <princeasingha@gmail.com>
2024-03-12 23:44:04 -0230

    done for today.

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,21 +1,51 @@
 "use client";
 
-import { LucideIcon } from "lucide-react";
+import { Id } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
+import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
 
 interface itemProps {
+  id?: Id<"documents">;
+  documentIcon?: string;
+  active?: boolean;
+  expanded?: boolean;
+  isSearch?: boolean;
+  level?: number;
+  onExpand?: () => void; // Nice use of optional function
   label: string;
-  onClick: () => void;
+  onClick: () => void; // Nice use of optional function
   icon: LucideIcon;
 }
 
-const Item = ({ label, onClick, icon: Icon }: itemProps) => {
+const Item = ({
+  id,
+  label,
+  onClick,
+  icon: Icon,
+  active,
+  documentIcon,
+  isSearch,
+  level = 0,
+  onExpand,
+  expanded,
+}: itemProps) => {
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
   return (
     <div
       onClick={onClick}
       role="button"
-      style={{ paddingLeft: "12px" }}
-      className="group min-h-[27px] text-sm py-1 pr-3 width-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
+      style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }} // Nice implementation of indentation
+      className={cn(
+        "group min-h-[27px] text-sm py-1 pr-3 width-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
+        active && "bg-primary/5 text-primary"
+      )}
     >
+      {!!id /*Turns to bool*/ && (
+        <div>
+          <ChevronDown />
+        </div>
+      )}
       <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
       <span className="truncate">{label}</span>
     </div>
